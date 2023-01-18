0ae5223bc8e7c04b47a3517d2b7807ed0e232d43
himanshu456ia <himanshu456ia@gmail.com>
2023-01-18 19:04:45 +0530

    modified

--- auditions/auditions/settings.py
+++ auditions/auditions/settings.py
@@ -149,6 +149,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL= "/media/"
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# import os
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
